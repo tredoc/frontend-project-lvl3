8baad02422d86eb3caebfee22509d84baa63cf12
tredok <wrk.dodge@gmail.com>
2020-06-23 17:09:09 +0300

    step #1 v3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -12,6 +13,11 @@ module.exports = {
         },
       },
     ],
+  },
+  entry: '/src/bin/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
