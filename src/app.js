aa2511986de90c40c0d31d2839b78ba162972a93
tredok <wrk.dodge@gmail.com>
2020-08-17 12:26:03 +0300

    refactoring v3

--- src/app.js
+++ src/app.js
@@ -8,69 +8,69 @@ import ru from './locales/ru';
 
 const UPDATE_PERIOD = 5000;
 
+const makeRequest = (feedUrl) => {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  return axios.get(`${proxy}${feedUrl}`);
+};
+
+const getFeed = (event, state) => {
+  event.preventDefault();
+  state.form.processState = 'sending'; // eslint-disable-line
+
+  makeRequest(state.form.rssRequest)
+    .then((response) => {
+      const feedObj = parse(response.data);
+      const errors = document.querySelector('parsererror');
+      if (errors) {
+        state.form.processError = 'parserError'; // eslint-disable-line
+      }
+      const feed = feedObj.feedData;
+      feed.id = Number(_.uniqueId());
+      feed.url = state.form.rssRequest;
+      const posts = feedObj.feedPosts.map((post) => ({ ...post, feedId: feed.id }));
+
+      state.feeds = [...state.feeds, feed]; // eslint-disable-line
+      state.posts = [...state.posts, ...posts]; // eslint-disable-line
+
+      state.form.processState = 'finished'; // eslint-disable-line
+      state.form.needUpdate = true; // eslint-disable-line
+      state.form.rssRequest = ''; // eslint-disable-line
+    })
+    .catch((err) => {
+      state.form.processState = 'failed'; // eslint-disable-line
+      if (err.response) {
+        console.log(err.response.status);
+      }
+    });
+};
+
+const updateFeed = (state, updatePeriod) => {
+  if (state.needUpdate) {
+    setTimeout(() => updateFeed(state, updatePeriod), updatePeriod);
+    return;
+  }
+
+  const feedIds = state.feeds.map((feed) => feed.id);
+  const promises = state.feeds.map((feed) => makeRequest(feed.url));
+  Promise.all(promises)
+    .then((data) => data.forEach((response, index) => {
+      const { feedPosts } = parse(response.data);
+      const postsWithId = feedPosts.map((post) => ({ ...post, feedId: feedIds[index] }));
+      const diff = _.differenceWith(postsWithId, state.posts, _.isEqual);
+      if (diff.length > 0) {
+        state.posts = [...diff, ...state.posts]; // eslint-disable-line
+      }
+    }))
+    .then(() => {
+      setTimeout(() => updateFeed(state, updatePeriod), updatePeriod);
+    });
+};
+
 const app = () => {
   const form = document.querySelector('form');
   const input = form.querySelector('input');
   const button = form.querySelector('button');
   const userMessage = document.querySelector('.user-message');
-
-  const makeRequest = (feedUrl) => {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    return axios.get(`${proxy}${feedUrl}`);
-  };
-
-  const getFeed = (event, state) => {
-    event.preventDefault();
-    state.form.processState = 'sending'; // eslint-disable-line
-
-    makeRequest(state.form.rssRequest)
-      .then((response) => {
-        const feedObj = parse(response.data);
-        const errors = document.querySelector('parsererror');
-        if (errors) {
-          state.form.processError = 'parserError'; // eslint-disable-line
-        }
-        const feed = feedObj.feedData;
-        feed.id = Number(_.uniqueId());
-        feed.url = state.form.rssRequest;
-        const posts = feedObj.feedPosts.map((post) => ({ ...post, feedId: feed.id }));
-
-        state.feeds = [...state.feeds, feed]; // eslint-disable-line
-        state.posts = [...state.posts, ...posts]; // eslint-disable-line
-
-        state.form.processState = 'finished'; // eslint-disable-line
-        state.form.needUpdate = true; // eslint-disable-line
-        state.form.rssRequest = ''; // eslint-disable-line
-      })
-      .catch((err) => {
-        state.form.processState = 'failed'; // eslint-disable-line
-        if (err.response) {
-          console.log(err.response.status);
-        }
-      });
-  };
-
-  const updateFeed = (state, updatePeriod) => {
-    if (state.needUpdate) {
-      setTimeout(() => updateFeed(state, updatePeriod), updatePeriod);
-      return;
-    }
-
-    const feedIds = state.feeds.map((feed) => feed.id);
-    const promises = state.feeds.map((feed) => makeRequest(feed.url));
-    Promise.all(promises)
-      .then((data) => data.forEach((response, index) => {
-        const { feedPosts } = parse(response.data);
-        const postsWithId = feedPosts.map((post) => ({ ...post, feedId: feedIds[index] }));
-        const diff = _.differenceWith(postsWithId, state.posts, _.isEqual);
-        if (diff.length > 0) {
-          state.posts = [...diff, ...state.posts]; // eslint-disable-line
-        }
-      }))
-      .then(() => {
-        setTimeout(() => updateFeed(state, updatePeriod), updatePeriod);
-      });
-  };
 
   i18next.init({
     lng: 'ru',
